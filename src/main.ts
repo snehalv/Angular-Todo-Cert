3bdd8718d2b07b78d473abe74ab5c5a3d3ab2dc3
snehalv <snehalv.2010@gmail.com>
2024-10-07 17:28:13 +0530

    app module bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
+import { AppModule } from './app/app.module';
+
 export class App {
   name = 'Angular';
 }
 
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule);
